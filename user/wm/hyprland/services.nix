c023d0aac9ae16397b6c93aae46f6228cd295f25
Hier0nim <hieronimdaniel@gmail.com>
2024-10-12 13:26:11 +0200

    move supend script directly to services file

--- user/wm/hyprland/services.nix
+++ user/wm/hyprland/services.nix
@@ -5,7 +5,13 @@
   ...
 }:
 let
-  inherit (import ./scripts.nix { inherit pkgs; }) suspendScript;
+  suspendScript = pkgs.writeShellScriptBin "script" ''
+    ${pkgs.pipewire}/bin/pw-cli i all 2>&1 | ${pkgs.ripgrep}/bin/rg running -q
+    # only suspend if audio isn't running
+    if [ $? == 1 ]; then
+      ${pkgs.systemd}/bin/systemctl suspend
+    fi
+  '';
 in
 {
   services = {
