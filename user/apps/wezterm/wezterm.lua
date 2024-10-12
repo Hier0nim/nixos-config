bca749894e2d4328c3af61cf30a5b6ac8de6dbb5
Hier0nim <hieronimdaniel@gmail.com>
2024-10-12 20:35:50 +0200

    remove unused wezterm keybindings

--- user/apps/wezterm/wezterm.lua
+++ user/apps/wezterm/wezterm.lua
@@ -47,13 +47,11 @@ c.font_size = 12
 
 -- Keybindings
 local act = wez.action
-local callback = wez.action_callback
 
 local mod = {
 	c = "CTRL",
 	s = "SHIFT",
 	a = "ALT",
-	l = "LEADER",
 }
 
 local keybind = function(mods, key, action)
@@ -62,58 +60,13 @@ end
 
 local keys = function()
 	local keys = {
-		-- CTRL-A, CTRL-A sends CTRL-A
-		keybind({ mod.l, mod.c }, "a", act.SendString("\x01")),
-
-		-- pane and tabs
-		keybind({ mod.l }, "v", act.SplitVertical({ domain = "CurrentPaneDomain" })),
-		keybind({ mod.l }, "h", act.SplitHorizontal({ domain = "CurrentPaneDomain" })),
-		keybind({ mod.l }, "z", act.TogglePaneZoomState),
-		keybind({ mod.l }, "c", act.SpawnTab("CurrentPaneDomain")),
-		keybind({ mod.l }, "j", act.ActivatePaneDirection("Left")),
-		keybind({ mod.l }, "k", act.ActivatePaneDirection("Down")),
-		keybind({ mod.l }, "l", act.ActivatePaneDirection("Up")),
-		keybind({ mod.l }, ";", act.ActivatePaneDirection("Right")),
-		keybind({ mod.l }, "x", act.CloseCurrentPane({ confirm = true })),
-		keybind({ mod.l }, "q", act.CloseCurrentTab({ confirm = true })),
-		keybind({ mod.l, mod.s }, "j", act.AdjustPaneSize({ "Left", 5 })),
-		keybind({ mod.l, mod.s }, "k", act.AdjustPaneSize({ "Down", 5 })),
-		keybind({ mod.l, mod.s }, "l", act.AdjustPaneSize({ "Up", 5 })),
-		keybind({ mod.l, mod.s }, ";", act.AdjustPaneSize({ "Right", 5 })),
-		keybind(
-			{ mod.l },
-			"e",
-			act.PromptInputLine({
-				description = wez.format({
-					{ Attribute = { Intensity = "Bold" } },
-					{ Foreground = { AnsiColor = "Fuchsia" } },
-					{ Text = "Renaming Tab Title...:" },
-				}),
-				action = callback(function(win, _, line)
-					if line == "" then
-						return
-					end
-					win:active_tab():set_title(line)
-				end),
-			})
-		),
-
-		-- workspaces
-		keybind({ mod.l }, "w", act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" })),
-
 		-- copy and paste
 		keybind({ mod.c, mod.s }, "c", act.CopyTo("Clipboard")),
 		keybind({ mod.c, mod.s }, "v", act.PasteFrom("Clipboard")),
 	}
-
-	-- tab navigation
-	for i = 1, 9 do
-		table.insert(keys, keybind({ mod.l }, tostring(i), act.ActivateTab(i - 1)))
-	end
 	return keys
 end
 
-c.leader = { mods = mod.c, key = "b", timeout_miliseconds = 2000 }
 c.keys = keys()
 
 return c
