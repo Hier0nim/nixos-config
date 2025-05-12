1e7386eb634db83b20fd676874fe13061e530b8a
Hier0nim <hieronimdaniel@proton.me>
2025-05-12 19:57:26 +0200

    enable 'withUWSM' hyprland option

--- hosts/common/optional/desktop-environment/hyprland.nix
+++ hosts/common/optional/desktop-environment/hyprland.nix
@@ -1,13 +1,15 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   programs.hyprland = {
     enable = true;
+    withUWSM = true;
   };
 
   xdg.portal = {
     enable = true;
     xdgOpenUsePortal = true;
     config = {
-      common.default = ["gtk"];
+      common.default = [ "gtk" ];
       hyprland.default = [
         "gtk"
         "hyprland"
@@ -21,7 +23,7 @@
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   # Allow hyprlock to unlock the screen
-  security.pam.services.hyprlock = {};
+  security.pam.services.hyprlock = { };
 
   programs.xwayland = {
     enable = true;
