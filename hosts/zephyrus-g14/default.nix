59bee45b94418cd23637016a3871d72bfe718d71
Hier0nim <hieronimdaniel@proton.me>
2025-11-17 19:45:26 +0100

    disable winboat temporarily

--- hosts/zephyrus-g14/default.nix
+++ hosts/zephyrus-g14/default.nix
@@ -80,7 +80,7 @@
     autoStart = true;
   };
 
-  custom.programs.winboat.enable = true;
+  custom.programs.winboat.enable = false;
 
   # https://wiki.nixos.org/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "25.05";
