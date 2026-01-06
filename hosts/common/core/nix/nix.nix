f98b3b8545a8d7c5cc99247d181474e0090ca3ad
Hier0nim <hieronimdaniel@proton.me>
2026-01-06 13:43:46 +0100

    fix vesktop

--- hosts/common/core/nix/nix.nix
+++ hosts/common/core/nix/nix.nix
@@ -42,11 +42,26 @@
     };
 
     overlays = [
-      # (final: prev: {
-      #   cosmic-comp = prev.cosmic-comp.overrideAttrs (old: {
-      #     patches = [ ./../../../../patches/no_ssd.patch ];
-      #   });
-      # })
+      (final: prev: {
+        vesktop = prev.vesktop.overrideAttrs (old: {
+          preBuild = ''
+            cp -r ${prev.electron.dist} electron-dist
+            chmod -R u+w electron-dist
+          '';
+          buildPhase = ''
+            runHook preBuild
+
+            pnpm build
+            pnpm exec electron-builder \
+              --dir \
+              -c.asarUnpack="**/*.node" \
+              -c.electronDist="electron-dist" \
+              -c.electronVersion=${prev.electron.version}
+
+            runHook postBuild
+          '';
+        });
+      })
     ];
   };
 
