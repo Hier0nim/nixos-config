2b7857bbc25b20f69e93519d9222938878093cc9
Hier0nim <hieronimdaniel@proton.me>
2026-01-06 18:38:22 +0100

    remove unecessary vesktop overlay

--- hosts/common/core/nix/nix.nix
+++ hosts/common/core/nix/nix.nix
@@ -42,26 +42,11 @@
     };
 
     overlays = [
-      (final: prev: {
-        vesktop = prev.vesktop.overrideAttrs (old: {
-          preBuild = ''
-            cp -r ${prev.electron.dist} electron-dist
-            chmod -R u+w electron-dist
-          '';
-          buildPhase = ''
-            runHook preBuild
-
-            pnpm build
-            pnpm exec electron-builder \
-              --dir \
-              -c.asarUnpack="**/*.node" \
-              -c.electronDist="electron-dist" \
-              -c.electronVersion=${prev.electron.version}
-
-            runHook postBuild
-          '';
-        });
-      })
+      # (final: prev: {
+      #   cosmic-comp = prev.cosmic-comp.overrideAttrs (old: {
+      #     patches = [ ./../../../../patches/no_ssd.patch ];
+      #   });
+      # })
     ];
   };
 
