add87119eb44e8900376399a6880c643438db91e
Hier0nim <hieronimdaniel@proton.me>
2026-05-12 21:52:18 +0200

    update providers

--- modules/home/common/programs/ai/coding-agents.nix
+++ modules/home/common/programs/ai/coding-agents.nix
@@ -6,8 +6,59 @@
 }:
 let
   ai = import ./data.nix;
+  modelsJsonFormat = pkgs.formats.json { };
+  modelsJson = {
+    providers = {
+      "${ai.api.local.providerId}" = {
+        inherit (ai.api.local) baseUrl;
+        api = "openai-completions";
+        apiKey = ai.api.local.providerId;
+        inherit (ai.api.local) compat;
+        models = [
+          {
+            id = ai.api.local.modelId;
+            name = ai.api.local.displayName;
+            reasoning = false;
+            input = [ "text" ];
+            inherit (ai.api.local) contextWindow;
+            maxTokens = 16384;
+            cost = {
+              input = 0;
+              output = 0;
+              cacheRead = 0;
+              cacheWrite = 0;
+            };
+          }
+        ];
+      };
+
+      "${ai.api.legion.providerId}" = {
+        inherit (ai.api.legion) baseUrl;
+        api = "openai-completions";
+        apiKey = "!${pkgs.coreutils}/bin/cat ${config.sops.secrets.${ai.api.legion.apiKeySecret}.path}";
+        inherit (ai.api.legion) authHeader compat;
+        models = [
+          {
+            id = ai.api.legion.modelId;
+            name = ai.api.legion.displayName;
+            reasoning = false;
+            input = [ "text" ];
+            inherit (ai.api.legion) contextWindow;
+            maxTokens = 16384;
+            cost = {
+              input = 0;
+              output = 0;
+              cacheRead = 0;
+              cacheWrite = 0;
+            };
+          }
+        ];
+      };
+    };
+  };
 in
 {
+
   sops.secrets.pi_legion_api_key = {
     sopsFile = config.custom.repoPath + "/secrets/server-legion/llama-cpp-agent.yaml";
     key = "llama_cpp_agent_api_key";
@@ -19,12 +70,10 @@ in
       codex
       inputs.serena.packages.${pkgs.stdenv.hostPlatform.system}.serena
       pi-coding-agent
+      socat
     ];
 
-    file.".pi/agent/settings.json".text = builtins.toJSON {
-      defaultProvider = "local-qwen";
-      defaultModel = "qwen";
-    };
+    file.".pi/agent/models.json".source = modelsJsonFormat.generate "models.json" modelsJson;
 
     file.".pi/agent/models.json".text = builtins.toJSON {
       providers = {
